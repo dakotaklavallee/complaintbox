436df590ce79551cc677ce7606bd097aa82f11d4
dakotaklavallee <dakota.k.lavallee@gmail.com>
2022-12-22 22:23:04 -0500

    first try

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Login from './lib/Login.svelte';
+  import Messages from './lib/Messages.svelte';
+</script>
+
+<h1>ComplaintBox</h1>
+
+<Login />
+<Messages />
+
+
---
+++ src/lib/Messages.svelte
@@ -0,0 +1,81 @@
+<script>
+    import { onMount, onDestroy } from 'svelte';
+    import {currentUser, pb} from './pocketbase';
+
+    let newMessage = "";
+    let messages = [];
+    let unsubscribe;
+
+    onMount(async () => {
+        const resultList = await pb.collection('messages').getList(1, 50, {
+            sort: 'created',
+            expand: 'user',
+        });
+        messages = resultList.items;
+
+        //subscribe to realtime messages
+        unsubscribe = await pb
+        .collection('messages')
+        .subscribe("*", async({ action, record }) => {
+            if (action === 'create'){
+                const user = await pb.collection('users').getOne(record.user);
+                record.expand = { user };
+                messages = [...messages, record];
+            }
+            if (action === 'delete'){
+                messages = messages.filter((m) => m.id !== record.id);
+            }
+        })
+    })
+
+    onDestroy(() => {
+        unsubscribe?.();
+    })
+
+    async function sendMessage() {
+        const data = {
+            text: newMessage,
+            user: $currentUser.id,
+        };
+        const createdMessage = await pb.collection('messages').create(data);
+    }
+    async function deleteMessage(id){
+        const deletedMessage = await pb.collection('messages').delete(id);
+    }
+</script>
+
+
+{#if $currentUser}
+<div class="messages">
+    {#each messages as message (message.id)}
+    <div class="msg">
+        <img
+        class="avatar"
+        src={`https://avatars.dicebear.com/api/identicon/${message.expand?.user?.username}.svg`}
+        alt="avatar"
+        width="40px"
+        />
+        <div>
+            <small>
+                Sent by @{message.expand?.user?.username}
+            </small>
+            <p class="msg-text">{message.text}</p>
+            {#if $currentUser.id === message.expand.user.id}
+            <button on:click={() => deleteMessage(message.id)}>Delete</button>
+            {/if}
+        </div>
+    </div>
+    {/each}
+</div>
+
+<form on:submit|preventDefault={sendMessage}>
+    <input
+    placeholder="Message"
+    type="text"
+    bind:value={newMessage}
+    />
+    <button type="submit">Send</button>
+</form>
+{:else}
+<div></div>
+{/if}
